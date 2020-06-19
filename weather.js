86c3e5fd9ebf50540ee03a8ad57cb8cc53ea4a79
HyoTaekJang <hyotaek9812@gmail.com>
2020-06-19 21:44:48 +0900

    success get coords and save coords

--- weather.js
+++ weather.js
@@ -1,5 +1,48 @@
-function init(){
+const  COORDS = "coords";
 
+function saveCurrentCoordsc(Obj){
+    localStorage.setItem(COORDS, JSON.stringify(Obj));
+}
+
+function handleGeoSucces(position){
+    console.log("succes for get Geo");
+    console.log(position);
+
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+
+    const coordsObj = {
+        latitude,
+        longitude
+    };
+
+    saveCurrentCoordsc(coordsObj);
+
+}
+
+function handleGeoError(){
+    console.log("Error for get Geo");
+}
+
+function askForCoords(){
+    navigator.geolocation.getCurrentPosition(handleGeoSucces, handleGeoError);
+   
+}
+
+function loadCoords(){
+    const currentCoords = localStorage.getItem(COORDS);
+
+    if(currentCoords == null){
+        askForCoords();
+    }
+    else{
+        // api 작동
+        console.log("success save coords");
+    }
+}
+
+function init(){
+    loadCoords();
 }
 
 init();
