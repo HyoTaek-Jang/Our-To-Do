0c2d2ebb1d939f9482397d052183e45183dd4696
HyoTaekJang <hyotaek9812@gmail.com>
2021-02-02 00:36:35 +0900

    [ADD&UPDATE] 프론트 css 다듬기, 리셋 로그인버튼 구현

--- greeting.js
+++ greeting.js
@@ -4,49 +4,49 @@ const greeting = document.querySelector(".main-welcome");
 const clearAll = document.querySelector(".clearAll");
 
 const USER_LS = "currentUser";
-const SHOWING_CN = "show"
+const SHOWING_CN = "show";
 
-function handleSubmit(event){
-    event.preventDefault();
-    const currentValue = input.value;
-    paintGreeting(currentValue);
-    saveName(currentValue);
+function handleSubmit(event) {
+  event.preventDefault();
+  const currentValue = input.value;
+  paintGreeting(currentValue);
+  saveName(currentValue);
 }
 
-function handleClear(event){
-    localStorage.clear();
-    location.reload()
+function handleClear(event) {
+  alert("clear");
+  // localStorage.clear();
+  // location.reload()
 }
 
-function askForName(){
-    form.classList.add(SHOWING_CN);
-    form.addEventListener("submit", handleSubmit);
-
+function askForName() {
+  form.classList.add(SHOWING_CN);
+  form.addEventListener("submit", handleSubmit);
 }
 
-function saveName(text){
-    localStorage.setItem(USER_LS, text);
+function saveName(text) {
+  localStorage.setItem(USER_LS, text);
 }
 
-function paintGreeting(text){
-    form.classList.remove(SHOWING_CN);
-    greeting.classList.add(SHOWING_CN);
-    greeting.innerHTML = `Enjoy your day, ${text} !`;
+function paintGreeting(text) {
+  form.classList.remove(SHOWING_CN);
+  greeting.classList.add(SHOWING_CN);
+  greeting.innerHTML = `Enjoy your day, ${text} !`;
 }
 
-function loadName(){
-    const currentUser = localStorage.getItem(USER_LS);
-    if(currentUser === null){
-        // none classList.remove(show) or add 로 show deshow 해버리기
-        askForName();
-    }else{
-         paintGreeting(currentUser);
-    }
+function loadName() {
+  const currentUser = localStorage.getItem(USER_LS);
+  if (currentUser === null) {
+    // none classList.remove(show) or add 로 show deshow 해버리기
+    askForName();
+  } else {
+    paintGreeting(currentUser);
+  }
 }
 
-function init(){
-    clearAll.addEventListener('click', handleClear);
-    loadName();
+function init() {
+  clearAll.addEventListener("click", handleClear);
+  loadName();
 }
 
 init();
