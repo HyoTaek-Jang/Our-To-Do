d77ded3d33617672c842e690aac20fc19018b3be
HyoTaek-Jang <hyotaek9812@gmail.com>
2022-03-05 14:45:28 +0900

    move: ourtodo 레거시 변경을 위한 파일 이동

---
+++ ourtodo2.0/withme/src/main/java/com/ourtodo/withme/WithmeApplication.java
@@ -0,0 +1,13 @@
+package com.ourtodo.withme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WithmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WithmeApplication.class, args);
+	}
+
+}
---
+++ ourtodo2.0/withme/src/test/java/com/ourtodo/withme/WithmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ourtodo.withme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WithmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
