7a2ebd5334a117bab2d3e1520c952ea39aa15c8c
HyoTaek-Jang <hyotaek9812@gmail.com>
2022-03-11 02:01:25 +0900

    style: 에러문구 수정

--- ourtodo2.0/withme/src/main/java/com/ourtodo/withme/domain/user/controller/AuthController.java
+++ ourtodo2.0/withme/src/main/java/com/ourtodo/withme/domain/user/controller/AuthController.java
@@ -1,6 +1,7 @@
 package com.ourtodo.withme.domain.user.controller;
 
 import javax.validation.Valid;
+import javax.xml.bind.ValidationException;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,7 +23,9 @@ public class AuthController {
 	private final SignupService signupService;
 
 	@PostMapping("/signup")
-	public ResponseEntity<? extends BaseResponse> signupUser(@Valid @RequestBody SignupRequest signupRequest) {
+	public ResponseEntity<? extends BaseResponse> signupUser(@Valid @RequestBody SignupRequest signupRequest) throws
+		ValidationException {
+		signupService.signupValid(signupRequest);
 		return null;
 	}
 
