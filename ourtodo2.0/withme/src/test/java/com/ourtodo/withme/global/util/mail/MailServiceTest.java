39289974edf05069727508e2a0ece3e1e9db056e
HyoTaek-Jang <hyotaek9812@gmail.com>
2022-03-24 00:30:16 +0900

    feat: 이메일 발송 테스트 코드 추가

---
+++ ourtodo2.0/withme/src/test/java/com/ourtodo/withme/global/util/mail/MailServiceTest.java
@@ -0,0 +1,25 @@
+package com.ourtodo.withme.global.util.mail;
+
+import javax.mail.MessagingException;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.ourtodo.withme.BaseTest;
+
+class MailServiceTest extends BaseTest {
+
+	private final MailService mailService;
+
+	@Autowired
+	public MailServiceTest(MailService mailService) {
+		this.mailService = mailService;
+	}
+
+	@Test
+	@DisplayName("이메일 발송 테스트")
+	void sendMailTest() throws MessagingException {
+		mailService.sendSignUpCertificationMail("gyxor8582@naver.com", "test code");
+	}
+}
