f304808920b8e0220f55211110db5bcb2481653e
HyoTaek-Jang <hyotaek9812@gmail.com>
2022-03-29 13:26:39 +0900

    fix: applicationTest activeProfile 설정 추가

--- src/test/java/com/ourtodo/withme/WithmeApplicationTests.java
+++ src/test/java/com/ourtodo/withme/WithmeApplicationTests.java
@@ -1,14 +1,14 @@
 package com.ourtodo.withme;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class WithmeApplicationTests {
 
 	@Test
-	@Disabled
 	void contextLoads() {
 	}
 
