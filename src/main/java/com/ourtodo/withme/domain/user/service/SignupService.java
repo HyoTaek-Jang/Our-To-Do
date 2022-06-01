f0bd241e5f063d8d80aa7c18a12af97b9891a685
HyoTaek-Jang <hyotaek9812@gmail.com>
2022-06-01 14:19:53 +0900

    feat: 회워가입시 기본 태그 생성

--- src/main/java/com/ourtodo/withme/domain/user/service/SignupService.java
+++ src/main/java/com/ourtodo/withme/domain/user/service/SignupService.java
@@ -1,11 +1,13 @@
 package com.ourtodo.withme.domain.user.service;
 
+import static com.ourtodo.withme.domain.tag.constants.TagConstants.*;
 import static com.ourtodo.withme.domain.user.constants.SignupValidationConstants.*;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.ourtodo.withme.domain.tag.service.TagService;
 import com.ourtodo.withme.domain.user.db.domain.Member;
 import com.ourtodo.withme.domain.user.db.repository.MemberRepository;
 import com.ourtodo.withme.domain.user.dto.request.SignupRequest;
@@ -19,6 +21,7 @@ public class SignupService {
 
 	private final MemberRepository memberRepository;
 	private final PasswordEncoder passwordEncoder;
+	private final TagService tagService;
 
 	@Transactional(readOnly = true)
 	public void signupValid(SignupRequest signupRequest) {
@@ -32,6 +35,8 @@ public class SignupService {
 	@Transactional
 	public Member saveUser(SignupRequest signupRequest) {
 		Member member = signupRequest.toEntity(passwordEncoder.encode(signupRequest.getPassword()));
-		return memberRepository.save(member);
+		Member savedMember = memberRepository.save(member);
+		tagService.addTag(savedMember, COMMON_TAG, COMMON_COLOR);
+		return savedMember;
 	}
 }
