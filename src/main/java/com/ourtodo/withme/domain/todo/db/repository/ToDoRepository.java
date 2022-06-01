e4dd78b3e92690fa2b886412761184a069597af1
HyoTaek-Jang <hyotaek9812@gmail.com>
2022-06-01 14:40:44 +0900

    feat: find last order by tag 추가

--- src/main/java/com/ourtodo/withme/domain/todo/db/repository/ToDoRepository.java
+++ src/main/java/com/ourtodo/withme/domain/todo/db/repository/ToDoRepository.java
@@ -1,8 +1,12 @@
 package com.ourtodo.withme.domain.todo.db.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.ourtodo.withme.domain.todo.db.domain.ToDo;
 
 public interface ToDoRepository extends JpaRepository<ToDo, Long> {
+	@Query(value = "SELECT IF(COUNT(ordering) > 0, MAX(ordering), 0) as lastOrder FROM to_do WHERE tag_id = :tag", nativeQuery = true)
+	Long findLastOrderByTag(@Param("tag")Long tag);
 }
